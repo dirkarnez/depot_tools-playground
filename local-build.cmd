573af1b3116b15a22c0e752b7651078dfac737ea
Dirk Arnez <smalldirkalex@gmail.com>
2024-11-09 20:44:43 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -8,7 +8,8 @@ SET PATH=^
 %DOWNLOADS_DIR%\PortableGit\bin;^
 %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
 %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
-%DOWNLOADS_DIR%\cmake-3.26.1-windows-x86_64\bin;
+%DOWNLOADS_DIR%\cmake-3.26.1-windows-x86_64\bin;^
+%DOWNLOADS_DIR%\depot_tools;
 
 @REM set PATH=^
 @REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
