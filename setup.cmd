2f2a2e057ef329516eebea790692ccda02e6808e
Dirk Arnez <smalldirkalex@gmail.com>
2024-11-09 20:39:53 +0800

    - update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,5 +1,14 @@
 @REM run as Administrator
 @echo off
+cd /d %~dp0
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
+
+SET PATH=^
+%DOWNLOADS_DIR%\PortableGit\bin;^
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+%DOWNLOADS_DIR%\cmake-3.26.1-windows-x86_64\bin;
 
 if not exist "%USERPROFILE%\Downloads\depot_tools" (
 cd /d "%TEMP%" && ^
